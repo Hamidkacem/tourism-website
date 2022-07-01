aa25bad9037c6a7028437670d3954e3204569a4c
Hamidkacem <hamidkacem4@gmail.com>
2022-07-01 04:21:48 +0100

    ex2/HomePage/Navbar

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Componts/NavBar/NavBar'
+import HomePage from './Componts/HomePage/HomePage'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <HomePage/>
+    </div>
+  );
+}
+
+export default App;
