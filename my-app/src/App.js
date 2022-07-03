c6d0d54ce8c934e2e5631309d83c9f37bb1ca19c
Hamidkacem <hamidkacem4@gmail.com>
2022-07-03 03:04:20 +0100

    responsive/NavBar/HomePage done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,18 @@
-import Navbar from './Componts/NavBar/NavBar'
-import HomePage from './Componts/HomePage/HomePage'
-import './App.css'
+import Header from '../src/Componts/Header/Header';
+import HomePage from '../src/Componts/HomePage/HomePage.jsx';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-     <Navbar/>
-     <HomePage/>
-    </div>
-  );
+
+
+const App = () =>{
+  return(
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route index element={<HomePage />} />
+        </Routes>
+      </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- my-app/src/Componts/HomePage/HomePage.jsx
+++ my-app/src/Componts/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./HomePage.css";
+
 function HomePage() {
   return (
     <div className="container">
       <div className="text-Explore">
         <span className="text-First-line"> So, you want to travel to</span>
         <span className="Space">Space</span>
-        Let’s face it; if you want to go to space, you might as well <br/>
+        Let’s face it; if you want to go to space, you might as well <br />
         <span>genuinely go to outer space and not hover kind of on the</span>
         edge of it. Well sit back, and relax because we’ll give you
         <span>a truly out of this world experience!</span>
